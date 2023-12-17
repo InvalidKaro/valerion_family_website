65229583b5c206185ad53e4c1e8d9181fa2c597b
Kalkata127 <kalkata125@gmail.com>
2023-12-17 16:15:14 +0200

    progress#1

--- react-frontend/src/header.js
+++ react-frontend/src/header.js
@@ -24,7 +24,6 @@ const Header = () => {
         /></a>
       </div>
     </header>
-    );
-}
- 
+    );}
+
 export default Header;
--- react-frontend/src/reviews.js
+++ react-frontend/src/reviews.js
@@ -4,71 +4,100 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Navigation, Pagination,} from 'swiper/modules';
 
 const Reviews = () => {
     return (
-
-            <section class="container reviews">
-                 <Swiper class="reviews"
-                    spaceBetween={0}
-                    slidesPerView={3}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
-                >
-                <SwiperSlide>
-                <div className="review__card">
-                    <img class="card__img"  src={review_profile_img} alt="" />
-                    <h1 className="card__username">Veskata</h1>
-                    <p className="card__review"><span>"</span>The best website for AI generated images. I made 10K in first month!<span>"</span></p>
-                    <div className="card__stars__sec">
-                        <div class="rating">
-                            <span class="star full"></span>
-                            <span class="star full"></span>
-                            <span class="star half"></span>
-                            <span class="star empty"></span>
-                            <span class="star empty"></span>
-                        </div>
+        <>
+        <section class="container">
+          <Swiper
+           slidesPerView={3}
+           spaceBetween={180}
+           centeredSlides={false}
+           navigation={true}
+           pagination={{
+             clickable: true,
+           }}
+           modules={[Navigation, Pagination]}
+           className="reviews-container"
+          >
+            <SwiperSlide className="review__card">
+                     <img class="card__img"  src={review_profile_img} alt="" />
+                     <h1 className="card__username">Veskata</h1>
+                     <p className="card__review"><span>"</span>The best website for AI generated images. I made 10K in first month!<span>"</span></p>
+                     <div className="card__stars__sec">
+                         <div class="rating">
+                             <span class="star full"></span>
+                             <span class="star full"></span>
+                             <span class="star half"></span>
+                             <span class="star empty"></span>
+                             <span class="star empty"></span>
+                         </div>
                     </div> 
-                </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                <div className="review__card">
-                    <img class="card__img"  src={review_profile_img} alt="" />
-                    <h1 className="card__username">Jordan</h1>
-                    <p className="card__review"><span>"</span>Suyyyyyyyy<span>"</span></p>
-                    <div className="card__stars__sec">
-                        <div class="rating">
-                            <span class="star full"></span>
-                            <span class="star full"></span>
-                            <span class="star half"></span>
-                            <span class="star empty"></span>
-                            <span class="star empty"></span>
-                        </div>
+             </SwiperSlide>
+             <SwiperSlide className="review__card">
+                     <img class="card__img"  src={review_profile_img} alt="" />
+                     <h1 className="card__username">Veskata</h1>
+                     <p className="card__review"><span>"</span>The best website for AI generated images. I made 10K in first month!<span>"</span></p>
+                     <div className="card__stars__sec">
+                         <div class="rating">
+                             <span class="star full"></span>
+                             <span class="star full"></span>
+                             <span class="star half"></span>
+                             <span class="star empty"></span>
+                             <span class="star empty"></span>
+                         </div>
                     </div> 
-                </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                <div className="review__card">
-                    <img class="card__img"  src={review_profile_img} alt="" />
-                    <h1 className="card__username">Dimoff</h1>
-                    <p className="card__review"><span>"</span>Maznaaaaaa<span>"</span></p>
-                    <div className="card__stars__sec">
-                        <div class="rating">
-                            <span class="star full"></span>
-                            <span class="star full"></span>
-                            <span class="star half"></span>
-                            <span class="star empty"></span>
-                            <span class="star empty"></span>
-                        </div>
+             </SwiperSlide>
+             <SwiperSlide className="review__card">
+                     <img class="card__img"  src={review_profile_img} alt="" />
+                     <h1 className="card__username">Veskata</h1>
+                     <p className="card__review"><span>"</span>The best website for AI generated images. I made 10K in first month!<span>"</span></p>
+                     <div className="card__stars__sec">
+                         <div class="rating">
+                             <span class="star full"></span>
+                             <span class="star full"></span>
+                             <span class="star half"></span>
+                             <span class="star empty"></span>
+                             <span class="star empty"></span>
+                         </div>
                     </div> 
-                </div>
-                </SwiperSlide>
-                </Swiper> 
-               
-                
-            </section>
-        
-    );
+             </SwiperSlide>
+             <SwiperSlide className="review__card">
+                     <img class="card__img"  src={review_profile_img} alt="" />
+                     <h1 className="card__username">Veskata</h1>
+                     <p className="card__review"><span>"</span>The best website for AI generated images. I made 10K in first month!<span>"</span></p>
+                     <div className="card__stars__sec">
+                         <div class="rating">
+                             <span class="star full"></span>
+                             <span class="star full"></span>
+                             <span class="star half"></span>
+                             <span class="star empty"></span>
+                             <span class="star empty"></span>
+                         </div>
+                    </div> 
+             </SwiperSlide>
+             <SwiperSlide className="review__card">
+                     <img class="card__img"  src={review_profile_img} alt="" />
+                     <h1 className="card__username">Veskata</h1>
+                     <p className="card__review"><span>"</span>The best website for AI generated images. I made 10K in first month!<span>"</span></p>
+                     <div className="card__stars__sec">
+                         <div class="rating">
+                             <span class="star full"></span>
+                             <span class="star full"></span>
+                             <span class="star half"></span>
+                             <span class="star empty"></span>
+                             <span class="star empty"></span>
+                         </div>
+                    </div> 
+             </SwiperSlide>
+          </Swiper>
+        </section>
+        </>
+      );
 }
  
 export default Reviews;
