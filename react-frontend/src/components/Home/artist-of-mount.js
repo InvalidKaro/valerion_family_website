e456c09199cdad85a6d94fe49d850816b57afbe6
InvalidKaro <stefanoprell09@gmail.com>
2024-01-05 09:22:50 +0100

    started working on AofM

--- react-frontend/src/components/Home/artist-of-mount.js
+++ react-frontend/src/components/Home/artist-of-mount.js
@@ -1,32 +1,54 @@
-import art_of_mount_img from '../../images/artOfMount.jpg'
-import artist_of_mount_img from '../../images/artistOfMount.jpg'
-
-const price = 50;
+import React, { useEffect, useState } from 'react';
+import art_of_mount_img from '../../images/artOfMount.jpg';
+import artist_of_mount_img from '../../images/artistOfMount.jpg';
 
 const AofM = () => {
-    return ( 
-        <section class="container aOfM">
-            <div class="aOfM__sec">
-             <div class="aOfM__header">
-                 <p>Art of the mount</p>
-                <div class="vertical-line"></div>
-                   <p>Artist of mount</p>
-             </div>
-             <div class="aofM__imgs">
-                <div class="img__box">
-                    <img src = {art_of_mount_img}></img>
-                    <div class="price">{price}$</div> 
-                </div>
-                <div class="img__box">
+  const [artOfMountData, setArtOfMountData] = useState([]);
+  const [artistOfMountData, setArtistOfMountData] = useState([]);
+  const price = 50;
 
-                <img src = {artist_of_mount_img}></img>
-                <div class="price">{price}$</div> 
-                </div>
-             </div>
+  useEffect(() => {
+    // Assume you have a backend API endpoint to fetch data
+    const fetchData = async () => {
+      try {
+        // Fetch data for "Art of the Mount"
+        const artOfMountResponse = await fetch('/api/artOfMount');
+        const artOfMountJson = await artOfMountResponse.json();
+        setArtOfMountData(artOfMountJson);
 
-            </div>
-        </section>
-    );
-}
- 
+        // Fetch data for "Artist of Mount"
+        const artistOfMountResponse = await fetch('/api/artistOfMount');
+        const artistOfMountJson = await artistOfMountResponse.json();
+        setArtistOfMountData(artistOfMountJson);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <section className="container aOfM">
+      <div className="aOfM__sec">
+        <div className="aOfM__header">
+          <p>Art of the mount</p>
+          <div className="vertical-line"></div>
+          <p>Artist of mount</p>
+        </div>
+        <div className="aofM__imgs">
+          <div className="img__box">
+            <img src={art_of_mount_img} alt="Art of the Mount" />
+            <div className="price">{price}$</div>
+          </div>
+          <div className="img__box">
+            <img src={artist_of_mount_img} alt="Artist of Mount" />
+            <div className="price">{price}$</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default AofM;
