a19abefdea0082ad093f82d1e30929774d85b21f
InvalidKaro <stefanoprell09@gmail.com>
2024-01-05 20:55:07 +0100

    Code takes art and artist from db

--- react-frontend/src/components/Home/artist-of-mount.js
+++ react-frontend/src/components/Home/artist-of-mount.js
@@ -1,29 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import art_of_mount_img from '../../images/artOfMount.jpg';
 import artist_of_mount_img from '../../images/artistOfMount.jpg';
-
 const AofM = () => {
   const [artOfMountData, setArtOfMountData] = useState([]);
   const [artistOfMountData, setArtistOfMountData] = useState([]);
-  const price = 50;
-  
+  const [artPrice, setArtPrice] = useState(0);
+  const [artistPrice, setArtistPrice] = useState(0);
   const fetchData = async () => {
     try {
       // Fetch data from localhost:80/month.php
       const response = await fetch('http://localhost:80/month.php');
       const json = await response.json();
-      
+
       // Filter the data based on the values of "Title"
       const artOfMountJson = json.filter((item) => item.Title === 'art');
       const artistOfMountJson = json.filter((item) => item.Title === 'artist');
-      
+
       setArtOfMountData(artOfMountJson);
       setArtistOfMountData(artistOfMountJson);
+
+      // Set the price from the filtered JSON data
+      if (artOfMountJson.length > 0) {
+        setArtPrice(artOfMountJson[0]?.price || 0);
+      }
+      
+      if (artistOfMountJson.length > 0) {
+        setArtistPrice(artistOfMountJson[0]?.price || 0);
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-  
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -38,12 +46,12 @@ const AofM = () => {
         </div>
         <div className="aofM__imgs">
           <div className="img__box">
-            <img src={art_of_mount_img} alt="Art of the Mount" />
-            <div className="price">{price}$</div>
+            <img src={artOfMountData[0]?.Art} alt="Art of the Mount" />
+            <div className="price">{artPrice}$</div>
           </div>
           <div className="img__box">
-            <img src={artist_of_mount_img} alt="Artist of Mount" />
-            <div className="price">{price}$</div>
+            <img src={artistOfMountData[0]?.Artist} alt="Artist of Mount" />
+            <div className="price">{artistPrice}$</div>
           </div>
         </div>
       </div>
