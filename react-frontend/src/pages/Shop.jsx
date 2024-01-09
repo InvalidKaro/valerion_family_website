c3e65899976b91859eabb535c4cebf1ef9b71329
InvalidKaro <stefanoprell09@gmail.com>
2024-01-09 13:11:28 +0100

    Removed unnecessary Modules

--- react-frontend/src/pages/Shop.jsx
+++ react-frontend/src/pages/Shop.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import '../styles/App.css';
-import Hero from '../components/Home/hero.js';
-import '../styles/hero.css';
-import AofM from '../components/Home/artist-of-mount.js';
-import '../styles/artist-of-mount.css';
-import Reviews from '../components/Home/reviews.js';
-import '../styles/reviews.css';
 import { useUser } from '../UserContext';
 
 function Home() {
