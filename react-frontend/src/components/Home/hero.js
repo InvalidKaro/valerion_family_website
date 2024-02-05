26c0289061e66934f79cd6265e4b0dbaca957ef8
InvalidKaro <stefanoprell09@gmail.com>
2024-02-05 21:41:57 +0100

    ds

--- react-frontend/src/components/Home/hero.js
+++ react-frontend/src/components/Home/hero.js
@@ -1,25 +1,65 @@
-// import hero_bg from '../../images/hero_bg.png';
+import React, { useEffect, useState } from 'react';
 import heroStyles from '../../styles/hero.module.css';
-const Hero = () => {
-    const sectionStyle = {
-        cursor: 'default',
-        WebkitTouchCallout: 'none',
-        WebkitUserSelect: 'none',
-        KhtmlUserSelect: 'none',
-        MozUserSelect: 'none',
-        msUserSelect: 'none',
-        userSelect: 'none',
 
-      };
-    return (
-            
-      <section style={sectionStyle} className={`${heroStyles.container} ${heroStyles.hero}`}>
-      <h1 className={heroStyles.hero__title}>Beyond Pixels: AI Artistry Market<wbr></wbr>place</h1>
-      <p className={heroStyles.hero__motto}>Your Dream, Our Work</p>
-      <p className={heroStyles.hero__description}>The most innovative place to buy and sell online art.</p>
-    </section>
-            
-    );
-}
- 
+const Hero = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('http://localhost:80/imageFetch.php');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Fetched images:', data); // Log the fetched data
+        setImages(data);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    
+    };
+
+    fetchImages();
+
+    const interval = setInterval(() => {
+      setCurrentImage(prevImage => (prevImage + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <main>
+    <section className={`${heroStyles.container} ${heroStyles.hero}`}>
+  <div className={heroStyles.imageContainer}>
+    {images.map((imageUrl, index) => (
+      <div
+        key={index}
+        className={`${heroStyles.heroImage} ${
+          index === currentImage ? heroStyles.visible : heroStyles.hidden
+        }`}
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+    ))}
+  </div>
+  <div className={heroStyles.frameOverlay}></div>
+  <div className={heroStyles.heroContent}>
+  <h1 className={`${heroStyles.heroTitle} ${heroStyles.smoothEffect} ${heroStyles.zoomEffect}`}>
+  Beyond Pixels
+</h1>
+<p className={`${heroStyles.heroMotto} ${heroStyles.smoothEffect}`}>
+  AI Artistry Marketplace
+</p>
+<p className={`${heroStyles.heroDescription} ${heroStyles.smoothEffect} ${heroStyles.pulsateEffect}`}>
+  Your Dream, Our Work
+</p>
+
+  </div>
+</section>
+    </main>
+  );
+};
+
 export default Hero;
