d27cbd2d1e23da58d979d774e0f4312f1617bdd7
InvalidKaro <stefanoprell09@gmail.com>
2024-04-06 18:37:09 +0200

    h

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+if (process.env.NODE_ENV === 'development') {
+  console.log("Running in development mode");
+  import("disable-react-error-overlay").then(() => {
+      const iframe = document.querySelector('iframe');
+      if (iframe) iframe.remove();
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
